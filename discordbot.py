22b6fd71b6c0737dae50c0bf958965fed5217f43
Hoshino Yu <89435441+HoshinoYu@users.noreply.github.com>
2021-08-24 12:02:38 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,37 +2,44 @@ import discord
 import googletrans
 import os
 from pprint import pprint
-# 輸入自己Bot的TOKEN碼
 TOKEN = os.environ['TOKEN']
 SRCLanguage=os.environ['SRC']
 DSTLanguage=os.environ['DST']
 
 client = discord.Client()
 
-# 起動時呼叫
 @client.event
 async def on_ready():
-    print('成功登入')
+    print('目前登入身份：',client.user)
+    game = discord.Game('少女前線')
+    await client.change_presence(status=discord.Status.idle, activity=game)
 
-# 收到訊息時呼叫
+
 @client.event
 async def on_message(message):
-    # 送信者為Bot時無視
     if message.author.bot:
         return
-    
-    if client.user in message.mentions: # @判定
+    #翻譯
+    if client.user in message.mentions:
         translator = googletrans.Translator()
         robotName = client.user.name
         first, space, content = message.clean_content.partition('@'+robotName+' ')
         
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
+    if content == '':
+        content = first
+    if translator.detect(content).lang == DSTLanguage:
+        return
+    if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+        remessage = translator.translate(content, dest='zh-tw').text
             await message.reply(remessage) 
-
-# Bot起動
+    #導覽        
+    if message.content == '!群組':
+        guilds = await client.fetch_guilds(limit=150).flatten()
+        for i in guilds:
+        await message.channel.send(i.name)
+    #人形強度    
+    if message.content.startswith('!人形強度'):
+        channel = message.channel
+        await channel.send('https://space.bilibili.com/3349725/article')
+        
 client.run(TOKEN)
