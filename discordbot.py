fef24c70b7b2d5205ec9c07a65e0d9b190ee4331
Hoshino Yu <89435441+HoshinoYu@users.noreply.github.com>
2021-08-24 12:41:31 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,13 @@
 import discord
 import googletrans
 import os
+import time
 from pprint import pprint
 TOKEN = os.environ['TOKEN']
 SRCLanguage=os.environ['SRC']
 DSTLanguage=os.environ['DST']
+!group=os.environ['gro']
+!T-Doll=os.environ['td']
 
 client = discord.Client()
 
@@ -31,6 +34,17 @@ async def on_message(message):
         return
     if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
         remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
+        await message.reply(remessage)
+            
+    #導覽        
+    if message.content == '!group':
+        guilds = await client.fetch_guilds(limit=150).flatten()
+        for i in guilds:
+        await message.channel.send(i.name)
+        
+    #人形強度    
+    if message.content.startswith('!T-Doll'):
+        channel = message.channel
+        await channel.send('https://space.bilibili.com/3349725/article')
         
 client.run(TOKEN)
